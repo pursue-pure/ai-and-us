3bc750ac2128152d850f195d5a3b029b8d9ac631
Drifting Frost <liushuangsanjin@gmail.com>
2026-04-22 16:39:15 +0000

    fix(lint): replace tabs in services exports

--- game/services/__init__.py
+++ game/services/__init__.py
@@ -4,8 +4,8 @@ from .checkpoint_service import CheckpointService, RespawnResult
 from .combat_service import CombatResult, CombatService
 
 __all__ = [
-	"CombatService",
-	"CombatResult",
-	"CheckpointService",
-	"RespawnResult",
+    "CombatService",
+    "CombatResult",
+    "CheckpointService",
+    "RespawnResult",
 ]
